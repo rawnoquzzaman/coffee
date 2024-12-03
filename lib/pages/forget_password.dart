03111431432948b4da6c81a3258fa1f7322e3811
rawnoquzzaman <zamanrawnoquz@gmail.com>
2024-12-03 15:58:33 +0600

    add

--- lib/pages/forget_password.dart
+++ lib/pages/forget_password.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
+import ' Reusable_background.dart';
 
 class ForgotPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Forgot Password")),
-      body: Center(child: Text("Forgot Password Page")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Enter your email to reset your password",
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Add your reset password logic here
+              },
+              child: Text("Reset Password"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
